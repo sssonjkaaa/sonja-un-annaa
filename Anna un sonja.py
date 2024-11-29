3ed015bbe132a28a963825f69200f31f4a676c2f
sssonjkaaa <afoninasonja@gmail.com>
2024-11-29 09:21:11 +0200

    Create Anna un sonja.py
    algoritms

---
+++ Anna un sonja.py
@@ -0,0 +1,37 @@
+1. Ievadīt skaitļus 
+
+2. Pārbaudīt, vai ievads ir derīgs skaitļu saraksts.
+
+3. Ja masīvs ir tukšs:
+
+   a) Izdrukāt "Masīvs ir tukšs, nevar veikt analīzi."
+
+   b) Beigt darbību.
+
+4. Ja masīvs nav tukšs:
+
+   a) Izdrukāt ievadītos skaitļus.
+
+   b) Sakārtot skaitļus augošā secībā, izmantojot Insertion Sort algoritmu.
+
+5. Veikt šādus aprēķinus:
+
+   a) Aprēķināt vidējo vērtību (aritmētisko vidējo).
+
+   b) Aprēķināt mediānu:
+
+      - Ja skaitļu skaits ir nepāra, mediāna ir vidējais skaitlis.
+
+      - Ja skaitļu skaits ir pāra, mediāna ir divu vidējo skaitļu vidējā vērtība.
+
+   c) Atrast vismazāko skaitli.
+
+   d) Atrast lielāko skaitli.
+
+   e) Aprēķināt skaitļu summu.
+
+6. Izdrukāt:
+
+   a) Sakārtotos skaitļus.
+
+   b) Statistikas rezultātus (vidējo vērtību, mediānu, vismazāko un lielāko skaitli, un skaitļu summu).
